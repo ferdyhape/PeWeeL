20af796a9633dacb68e8f17138a04c2c275ef170
ferdyhape <ferdyhahan5@gmail.com>
2022-04-06 00:43:02 +0700

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Route::get('/cek', function () {
+    return view('cek');
+});
+
+Route::resource('/home', ProductController::class);
+
+Auth::routes();
